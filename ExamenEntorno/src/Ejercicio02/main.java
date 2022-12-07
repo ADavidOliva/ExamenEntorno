410410d025ebd9805eee14a535ab3f7e071c1226
khaler.012 <khaler.012@mbp-de-angel>
2022-12-07 20:21:42 +0100

    main

---
+++ ExamenEntorno/src/Ejercicio02/main.java
@@ -0,0 +1,12 @@
+package Ejercicio02;
+
+public class main {
+
+	public static void main(String[] args) {
+		// creamos un 'syso' que contiene hola mundo
+		
+		System.out.println("Hola mundo");
+
+	}
+
+}

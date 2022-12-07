ae16884f567b893dd0573139043846af8a545849
khaler.012 <khaler.012@mbp-de-angel>
2022-12-07 19:48:32 +0100

    creamos un 'syso' que contiene hola mundo

--- ExamenEntorno/src/Ejercicio02/main.java
+++ ExamenEntorno/src/Ejercicio02/main.java
@@ -3,9 +3,9 @@ package Ejercicio02;
 public class main {
 
 	public static void main(String[] args) {
-		//creamos un 'syso' hola mundo
+		// creamos un 'syso' con un hola mundo
 		
-		System.out.println("Hola mundo");
+		System.out.println("hola mundo");
 
 	}
 
